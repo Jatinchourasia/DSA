41609b1b20ea85b73dc323d3cf7e4bb55151126f
Jatinchourasia <jatinchourasia3@gmail.com>
2021-12-13 08:29:10 +0530

    solution with complexity of on

--- LongestConsecutiveSequence.cpp
+++ LongestConsecutiveSequence.cpp
@@ -19,3 +19,50 @@ int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     }
     return ans;
 }
+
+
+// o(n2) 
+
+
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+  
+  sort(arr.begin(),arr.end());
+  maxLen = 1;
+  sequence =1;
+  for(int i = 0; i<arr.size();i++){
+      if(arr[i]==arr[i-1]+!)sequence++;
+      else if(arr[i]==arr[i-1])continue;
+      else sequence=1;
+
+      maxLen = max(maxLen, sequence)
+  }
+  return maxLen;
+}
+
+
+// o(n) using unordered set
+
+#include<bits/stdc++.h>
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+    unordered_set<int> box;
+        int ans = 1;
+    
+    for(int i=0;i<n;i++){
+        box.insert(arr[i]);
+    }
+    
+    for(int i=0;i<n;i++){
+ 
+       int start = arr[i];
+//         if start not exist in box it will return end value of box thats what find return
+        if(box.find(start-1) != box.end())continue;
+        while(box.find(start) != box.end()){
+            start++;
+        }
+        ans = max(ans,start-arr[i]);
+        
+    }
+    return ans;
+   
+    
+}
